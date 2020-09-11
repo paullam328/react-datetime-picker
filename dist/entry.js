5f5ad60ac335ac287a79c6e070cc825295b2a468
Paul Lam <paul.lam@burli.com>
2020-09-11 13:07:14 -0700

    added default export back

---
+++ dist/entry.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DateTimePicker", {
+  enumerable: true,
+  get: function get() {
+    return _DateTimePicker["default"];
+  }
+});
+
+require("react-calendar/dist/Calendar.css");
+
+require("react-clock/dist/Clock.css");
+
+var _DateTimePicker = _interopRequireDefault(require("./DateTimePicker"));
+
+require("./DateTimePicker.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
